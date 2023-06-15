0da6701aedea66bb21497e8e14a407ae483faa85
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2023-06-15 13:38:22 +0200

    getting ready for books hosting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // * Import the needed packages
 // *    - need to make the project "type": "module" in `package.json`
-import express from 'express'
+import express, { json } from 'express'
+import { getBooks } from './utils.js'
 
 // ? Create a server instance - invoke the express method
 const server = express()
@@ -12,6 +13,33 @@ server.listen(PORT, () => {
 })
 
 // ? We can the assign endpoint for the server to listen to requests to
+
+// ? GET listener at: / (a.k.a root/home)
 server.get('/', (request, response) => {
-    response.json({ message: 'Welcome to the TDL Library! 📚' })
+    response.json({ message: 'Hello! Welcome to the TDL Library! 📚' })
+})
+
+// ? Listen to a request for a books query - Search & Filter
+// ? GET listener at: /search
+server.get('/search', (request, response) => {
+    // ? We need to get the query param out of the request
+    const { query } = request
+
+    // ? We will assume that only 1 query param will be passed
+    // ? Meaning we will only search & filter by 1 category
+    const keys = Object.keys(query)
+
+    getBooks()
+
+    // ? If no query is passed in, return the entire list of books
+    if (keys.length === 0) {
+
+    } else {
+        // ? Otherwise, return the filtered list, filtered by the category provided from the query
+        const [key, ] = keys
+
+
+    }
+
+    response.json(keys)
 })
